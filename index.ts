ca4a19eb3193bf3145a591a7281953e16cf69a45
Bernard Rodrigue <brodrigue@audiokinetic.com>
2017-11-07 08:28:45 -0500

    scrist is able to create random objects!

--- index.ts
+++ index.ts
@@ -1,39 +1,50 @@
 import * as waapi from 'waapi-client';
 import { ak } from 'waapi';
+import * as words from './words';
 
-function onNameChanged(args?: any[], kwargs?: any) {
-    // Just received a notification about the name changed
-    console.log(`name changed: ${kwargs.object.id}: ${kwargs.newName}`);
-}
+const types = ['WorkUnit', 'ActorMixer','RandomSequenceContainer','Sound'];
 
 async function main() {
 
     try {
         // Connect to WAAPI
         // Ensure you enabled WAAPI in Wwise's User Preferences
-        var connection = await waapi.connect('ws://localhost:8080/waapi');
+        var session = await waapi.connect('ws://localhost:8080/waapi');
 
         // Obtain information about Wwise
-        var wwiseInfo = await connection.call(ak.wwise.core.getInfo, {});
+        var wwiseInfo = await session.call(ak.wwise.core.getInfo, {});
         console.log(`Hello ${wwiseInfo.displayName} ${wwiseInfo.version.displayName}!`);
 
-        // Subscribe to name changes
-        var subscription = await connection.subscribe(ak.wwise.core.object.nameChanged, onNameChanged, {});
+        
+        async function populate(parent:string, parentType:string){
+            const objTypeFrom = Math.max(1,types.indexOf(parentType));
+            const childCount = Math.floor(Math.random()*10)+8;
+            for (var index = 0; index < childCount; index++) {
+                const type = types[objTypeFrom+Math.floor(Math.random()*(types.length-objTypeFrom))];
+                const path = await create(parent, type);
 
-        // Create a new Wwise Sound object in the default work unit
-        var created = await connection.call(ak.wwise.core.object.create, {
-            parent: "\\Actor-Mixer Hierarchy\\Default Work Unit",
-            type: "Sound",
-            name: "MySound",
-            onNameConflict:"rename" }
-        );
+                if(type != 'Sound'){
+                    await populate(path, type);
+                }                
+            }
+        }
+        async function create(parent:string, type:string):Promise<string>{
+            const name = words.createName(Math.floor(Math.random()*4)+1);
+
+            // Create a new Wwise Sound object in the default work unit
+            await session.call(ak.wwise.core.object.create, {
+                parent, type, name,
+                onNameConflict:"rename" }, {});
+
+            return parent + '\\' + name;
+        }        
+        await populate("\\Actor-Mixer Hierarchy\\Default Work Unit", 'WorkUnit');
 
         // Disconnect everything
-        await connection.unsubscribe(subscription);
-        await connection.disconnect();
+        await session.disconnect();
     }
     catch (e) {
-        console.log(e);
+        console.log(JSON.stringify(e,null,4));
     }
 
     process.exit();
